f721ef218e1a48a799d2db0229427f1537cdb190
fl0pp5 <fl0pp5@mail.ru>
2022-06-14 03:53:21 +0300

    added example

---
+++ examples/get_addr.c
@@ -0,0 +1,43 @@
+#include <string.h>
+
+#include "../src/flajok.h"
+
+/* e.g.
+ * gcc -o getaddr get_addr.c
+ * ./getaddr port=4040 --ip=192.12.55.10
+ */
+
+int
+main(int argc, char **argv) {
+
+    if (argc < 2) {
+        return -1;
+    }
+
+    int port;
+    char *ip;
+    size_t i;
+
+    flag_t keys[] = {
+            (flag_t) {
+                .name="ip",
+                .type=STR_FLAG,
+            },
+            (flag_t) {
+                .name="port",
+                .type=INT_FLAG,
+            },
+    };
+    size_t n = 2;
+    flag_set_t flag_set = flajok(argc, argv, keys, n);
+
+    for (i = 0; i < flag_set.len; i++) {
+        if (strcmp(flag_set.flags[i].name, "port") == 0) {
+            port = *(int *) flag_set.flags[i].value;
+        } else {
+            ip = flag_set.flags[i].value;
+        }
+    }
+
+    printf("address: %s:%d\n", ip, port);
+}
