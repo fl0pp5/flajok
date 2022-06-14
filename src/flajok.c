21e19a904cef9590eee213cd26f76f5e29a4cc5c
fl0pp5 <fl0pp5@mail.ru>
2022-06-14 03:26:43 +0300

    flajok source code

---
+++ src/flajok.c
@@ -0,0 +1,167 @@
+#include <ctype.h>
+#include <string.h>
+
+#include "flajok.h"
+
+void
+set_flag_value(flag_set_t *flag_set, raw_flag_t source, size_t index) {
+    switch (flag_set->flags[index].type) {
+        case INT_FLAG:
+        case BOOL_FLAG:
+            *(int *) flag_set->flags[index].value = strtol(source.value, NULL, 10);
+            break;
+        case STR_FLAG:
+            strcpy(flag_set->flags[index].value, source.value);
+            break;
+        default:
+            break;
+    }
+}
+
+size_t
+parse_one(flag_set_t *flag_set, const char *word, size_t start) {
+    size_t
+        word_len            = strlen(word),
+        i_word              = start,
+        flag_id,
+        i;
+
+    raw_flag_t raw_flag     = {};
+    char       ch;
+
+    /* get minuses */
+    if (word[i_word] == '-') {
+        if (word[i_word + 1] == ' ' || isdigit(word[i_word + 1])) {
+
+            fputs("invalid format", stderr);
+            return -1;
+
+        } else if (word[i_word + 1] == '-') {
+            i_word++;
+        }
+        i_word++;
+    }
+
+    /* get name */
+    for (i = 0; i_word < word_len; i_word++, i++) {
+        ch = word[i_word];
+
+        if (ch == ' ' || ch == '=') {
+            i_word++;
+            break;
+        }
+
+        raw_flag.name[i] = ch;
+    }
+
+    flag_id = get_flag_id(flag_set, raw_flag.name);
+
+    /* check flag exists */
+    if (flag_id == -1) {
+        fprintf(stderr,"flag `%s` does not exists\n", raw_flag.name);
+        return -1;
+    }
+
+    /* check if type is bool */
+    if (flag_set->flags[flag_id].type == BOOL_FLAG) {
+        return i_word;
+    }
+
+    /* else. get value */
+
+    for (i = 0; i_word < word_len; i_word++, i++) {
+        ch = word[i_word];
+
+        if (isspace(ch) || ch == '\0') {
+            raw_flag.value[i] = '\0';
+            i_word++;
+            break;
+        }
+
+        if (ch == '-' || !isalpha(ch) && !isdigit(ch) && !ispunct(ch)) {
+            fprintf(stderr,"invalid symbol : `%c` \n", ch);
+            return -1;
+        }
+
+        raw_flag.value[i] = ch;
+    }
+
+    set_flag_value(flag_set, raw_flag, flag_id);
+
+    return i_word;
+}
+
+flag_set_t
+make_flag_set() {
+    return (flag_set_t) {
+            .len=0,
+            .cap=FLAG_SET_LEN,
+    };
+}
+
+size_t
+get_flag_id(flag_set_t *flag_set, const char *name) {
+    size_t i;
+
+    for (i = 0; i < flag_set->len; i++) {
+        if (strcmp(flag_set->flags[i].name, name) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int
+add_flag(flag_set_t *flag_set, flag_t flag) {
+    if (get_flag_id(flag_set, flag.name) != -1) {
+        return -1;
+    }
+
+    flag_set->flags[flag_set->len] = flag;
+
+    flag_set->len++;
+
+    return 1;
+}
+
+void
+extract_word(int argc, char **argv, char word[]) {
+    size_t i;
+
+    for (i = 1; i < argc; i++) {
+        strcat(word, strcat(argv[i], " "));
+    }
+
+}
+
+flag_set_t
+flajok(int argc, char **argv, flag_t keys[], size_t keys_len) {
+    char       src[ARGV_BUF_LEN] = {};
+    extract_word(argc, argv, src);
+
+    flag_set_t flag_set = make_flag_set();
+    size_t
+            src_len         = strlen(src),
+            i               = 0,
+            step            = 0;
+
+    for (i = 0; i < keys_len; i++) {
+        if (add_flag(&flag_set, keys[i]) == -1) {
+            return (flag_set_t) {
+                    .len=-1,
+            };
+        }
+    }
+
+    while (step < src_len) {
+        step = parse_one(&flag_set, src, step);
+
+        if (step == -1) {
+            return (flag_set_t) {
+                    .len=-1,
+            };
+        }
+    }
+
+    return flag_set;
+}
---
+++ src/flajok.h
@@ -0,0 +1,56 @@
+#ifndef FLAJOK_FLAJOK_H
+#define FLAJOK_FLAJOK_H
+#include <stdlib.h>
+#include <stdio.h>
+
+#define NAME_BUF_LEN  64
+#define VALUE_BUF_LEN 128
+#define ARGV_BUF_LEN 1024
+
+#define FLAG_SET_LEN  32
+
+enum FlagType {
+    INT_FLAG,
+    STR_FLAG,
+    BOOL_FLAG,
+};
+
+typedef struct {
+    enum FlagType type;
+    const char *name;
+    const char *usage;
+    char  value[VALUE_BUF_LEN];
+    const char *default_value;
+} flag_t;
+
+
+typedef struct {
+    char name[NAME_BUF_LEN];
+    char value[VALUE_BUF_LEN];
+} raw_flag_t;
+
+
+typedef struct {
+    flag_t flags[FLAG_SET_LEN];
+    size_t len;
+    size_t cap;
+} flag_set_t;
+
+size_t
+parse_one(flag_set_t *flag_set, const char *word, size_t start);
+
+flag_set_t
+make_flag_set();
+
+int
+add_flag(flag_set_t *flag_set, flag_t flag);
+
+size_t
+get_flag_id(flag_set_t *flag_set, const char *name);
+
+
+
+flag_set_t
+flajok(int argc, char **argv, flag_t keys[], size_t keys_len);
+
+#endif //FLAJOK_FLAJOK_H
